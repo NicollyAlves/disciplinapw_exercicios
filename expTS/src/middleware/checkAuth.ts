a9e81963a1e734225aade3755dc2c601ddc610c7
NicollyAlves <nicollyalvesdasilva12@gmail.com>
2024-07-23 11:44:10 -0400

    expTS finished

--- expTS/src/middleware/checkAuth.ts
+++ expTS/src/middleware/checkAuth.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Request, Response } from "express"
 
 
-
 export const checkAuth = (req: Request, res: Response, next: NextFunction) => {
     if(!req.session.uid) res.redirect("/auth/login")
     else next()
